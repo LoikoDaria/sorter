8ba76674e6f7fbf96fdfb626ae58df2471b29c2d
LoikoDaria <36641426+LoikoDaria@users.noreply.github.com>
2018-02-26 18:49:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 class Sorter {
   constructor() {
     // your implementation
+	this.array=new Array();
   }
 
   add(element) {
     // your implementation
+	this.array.push(element);
   }
 
   at(index) {
     // your implementation
+	return this.array[index];
   }
 
   get length() {
     // your implementation
+	return this.array.length;
   }
 
   toArray() {
     // your implementation
+	return this.array;
   }
 
   sort(indices) {
     // your implementation
+	var tempArray=new Array(); 
+	for(var i=0;i<indices.length;i++){ 
+		tempArray.push(this.array[indices[i]]); 
+	} 
+	tempArray.sort(); 
+	for(var i=0;i<indices.length;i++){ 
+		this.array[indices[i]]=tempArray[i];
+	}
+
   }
 
   setComparator(compareFunction) {
